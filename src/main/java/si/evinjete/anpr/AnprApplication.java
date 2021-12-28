41f9e7bfa3ed14cebbf0bbd1de1d442373d52f51
lovrovrazic <lovro.vrazic@gmail.com>
2021-12-28 15:23:22 +0100

    Registering with etcd added

--- src/main/java/si/evinjete/anpr/AnprApplication.java
+++ src/main/java/si/evinjete/anpr/AnprApplication.java
@@ -1,8 +1,11 @@
 package si.evinjete.anpr;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@RegisterService
 @ApplicationPath("v1")
 public class AnprApplication extends Application {
 }
